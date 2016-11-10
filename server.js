bd504d420eb34d5d8e386645f5e8dec1fbf966e2
Gavin96 <2831730038@qq.com>
2016-11-10 17:26:27 +0800

    hereby, could check the path and perform accordingly

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 var http = require("http");      //请求Node.js自带的http模块，并将其赋值给http变量
 var url = require("url");
 
-function start(route){
+function start(route,handle){ //add the parameter route and handle 
 	function onRequest(request, response){       //createServer函数会返回一个对象
-		console.log("Request received");
+		// console.log("Request received");
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 
-		route(pathname);      //将路由函数作为参数传递过去
+		route(handle,pathname);      //将路由函数作为参数传递过去
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hi World ~");
