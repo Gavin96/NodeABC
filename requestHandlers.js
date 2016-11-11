2f474ca9ab99b65294c0b61a518ef8b9b7113128
Gavin96 <2831730038@qq.com>
2016-11-11 13:51:19 +0800

    to confirm timecost op in /start will not disturb responds to /upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -9,19 +9,21 @@ function start(response){
 	console.log("Request handler 'start' was called.");	
 
 	// sleep(10000);         //waits for 10s
-	exec("ls -lah", function(error, stdout, stderr){
-		response.writeHead(200, {"Conten-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
-	return "Hello Start";       //让请求处理程序返回需要在浏览器中显示的信息
+	// exec("ls -lah", function(error, stdout, stderr){
+		exec("find /",{timeout: 10000, maxBuffer: 20000*1024},
+		    function(error, stdout, stderr){
+			 response.writeHead(200, {"Content-Type": "text/plain"});
+			 response.write(stdout);
+			 response.end();
+		});
+	// return "Hello Start";       //让请求处理程序返回需要在浏览器中显示的信息
 } 
 
 function upload(response) {	
 	console.log("Request handler 'upload' was called.");
 
 	// sleep(10000);    
-	response.writeHead(200, {"Conten-Type": "text/plain"});
+	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello Upload");
 	response.end();
 	// return "Hello Upload";
