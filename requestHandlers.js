eca6a9529b6dccb499b6fb749abefb682b72ab0e
Gavin96 <2831730038@qq.com>
2016-11-11 10:45:17 +0800

    when sth costs time for browser to respond

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,22 @@
 function start(){
 	console.log("Request handler 'start' was called.");
+
+	function sleep(milliSeconds){              //set the func to make program waits
+		var startTime = new Date().getTime();
+		while(new Date().getTime() < startTime + milliSeconds);
+	}
+
+	sleep(10000);         //waits for 10s
 	return "Hello Start";       //让请求处理程序返回需要在浏览器中显示的信息
 } 
 
 function upload() {
+	function sleep(milliSeconds){              //set the func to make program waits
+		var startTime = new Date().getTime();
+		while(new Date().getTime() < startTime + milliSeconds);
+	}
+
+	sleep(10000);    
 	console.log("Request handler 'upload' was called.");
 	return "Hello Upload";
 }
