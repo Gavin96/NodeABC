5e4edee72091edecee9987b7790b082f609d6786
Gavin Wang <gavin.wang@oocl.com>
2016-12-23 16:30:56 +0800

    writefile and open and import selfmade module

--- NaiveBird/writefile.js
+++ NaiveBird/writefile.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+// var compress = require("./compress.js");
+var s = require('./sleep.js');
 
 console.log("Ready to write in...");
 fs.writeFile('writetest.txt','我是要写入的内容', function(err) {
@@ -12,5 +14,53 @@ fs.writeFile('writetest.txt','我是要写入的内容', function(err) {
 			return console.error(err);
 		}
 		console.log("asyn data read: " + data.toString());
+	});
+});
+// fd 通过方法返回的文件描述符
+fs.open('test1.txt','r+',function(err, fd){
+	if (err) {
+		return console.error(err);
+	}
+	console.log("file open ok.");
+});
+
+
+console.log('\n');
+s.sleep(4000);
+// compress.sleep(3000);
+// 截取文件
+var buf = new Buffer(1024);
+
+fs.open('test2.txt','r+',function(err, fd) {
+	if (err){
+		return console.error(err);
+	}
+	console.log("file open ok.");
+	console.log("get contents after 10th character.");
+
+	fs.ftruncate(fd, 10, function(err){
+		if (err){
+			console.log(err);
+		}
+		console.log("file intercept right.");
+		console.log("read the same file.");
+		fs.read(fd, buf, 0, buf.length, 0, function(err,bytes){
+			if (err) {
+				console.log(err);
+			}
+
+			// layout read characters
+			if (bytes > 0){
+				console.log(buf.slice(0, bytes).toString());
+			}
+
+			// close file
+			fs.close(fd, function(err){
+				if (err) {
+					console.log(err);
+				}
+				console.log("file close ok.");
+			});
+		});
 	});
 });
